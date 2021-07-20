8c44dc74584d72eb0de03b75ae05b0b6da9784ee
haykel Mahfoudhi <haykell.mahfoudhi@gmail.com>
2021-07-21 00:15:27 +0100

    2 . Services Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,6 +1,12 @@
 //pages components
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/servicesSection";
 const AboutUs = () => {
-  return <AboutSection />;
+  return (
+    <>
+      <AboutSection />
+      <ServicesSection />
+    </>
+  );
 };
 export default AboutUs;
