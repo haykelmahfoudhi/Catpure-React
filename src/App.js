462c4dabd9754349a7bf4e1bc3f40a97ca0b6b25
haykel Mahfoudhi <haykell.mahfoudhi@gmail.com>
2021-07-21 21:18:38 +0100

    React router pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 //Import Pages
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+//Import Components
+import Nav from "./components/Nav";
 //Global Style
 import GlobalStyle from "./components/GlobalStyle";
+//Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <AboutUs />
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
