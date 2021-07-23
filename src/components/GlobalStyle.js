2b16bb48dce7e8316f67cee3b5148608a4814565
haykel Mahfoudhi <haykell.mahfoudhi@gmail.com>
2021-07-24 00:01:16 +0100

    Mobile Friendly

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -6,6 +6,14 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
 }
+
+html{
+    @media(max-width:1700px){
+        font-size:75%
+    }
+    
+    
+}
 body{
   background  :#1b1b1b ;
   font-family: 'Inter', sans-serif;
@@ -50,6 +58,7 @@ button{
         font-size: 1.4rem;
         line-height: 150%;
     }
+
 `;
 
 export default GlobalStyle;
